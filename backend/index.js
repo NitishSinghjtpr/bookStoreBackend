750c748dba048b354dfd50cca50572598778eabc
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-02-09 20:45:36 +0530

    connect course ui to backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import bookRoute from "./Route/book.route.js";
+import cors from "cors";
 
-dotenv.config();
+dotenv.config();        // 1️⃣ Load environment variables
 
-const app = express();
-connectDB();
+const app = express();  // 2️⃣ Create express app
 
-const PORT = process.env.PORT || 5000;
+app.use(cors());        // 3️⃣ Middlewares
+app.use(express.json());
 
+connectDB();            // 4️⃣ Connect Database (after env loaded)
+
+// 5️⃣ Routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.use("/book", bookRoute);
+
+// 6️⃣ Start Server
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is listing at port:${PORT}`);
+  console.log(`Server is running at port: ${PORT}`);
 });
